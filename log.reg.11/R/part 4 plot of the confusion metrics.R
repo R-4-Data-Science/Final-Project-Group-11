3e88ca804cb28b0d4059386c0f1a2f228e59d108
RongjingXia <rzx0012@auburn.edu>
2023-11-29 22:14:36 -0600

    Update part 4 plot of the confusion metrics.R

--- log.reg.11/R/part 4 plot of the confusion metrics.R
+++ log.reg.11/R/part 4 plot of the confusion metrics.R
@@ -46,8 +46,6 @@ plot_confusion_matrix <- function(y, X, beta, value_name = "prevalence"){
       cat(value_name, "do not exist.\n")
     }
 }   
-  
-  
-  
-   
-   
+
+
+# Reference：https://chat.openai.com/share/d17d0b56-4d16-4200-8ab9-668d61c9ac1b

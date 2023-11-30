ab4b84512e11f825405363ca27253438f89ca45a
Charles Benfer <czb0146@auburn.edu>
2023-11-30 10:04:10 -0600

    Made some edits to the confusion matrix r code and the confusion matrix plotting r code

--- log.reg.11/R/part 3 confusion matrix.R
+++ log.reg.11/R/part 3 confusion matrix.R
@@ -1,53 +1,59 @@
 #' @title “Confusion Matrix”  (using a cut-off value for prediction at 0.5)
 #' @description based on this cut-off value, create the confusion matrix and also output the following metrics: prevalence, accuracy, sensitivity, specificity, false discovery rate, and diagnostic odds ratio
-#' @param  beta: A  \code{matrix} to denote the coefficient vector.
-#' @param X:  A  \code{coloumn vector} containing the predictor.
-#' @param y : A  \code{coloumn vector} containing the responses.
+#' @param  Data: A  \code{matrix} containing the data we want to perform logistic regression on.
+#' @param X:  A  \code{vector} denoting the columns of \code{Data} representing the predictor variables.
+#' @param y : A  \code{integer} denoting the column of \code{Data} representing the response variable.
 #' @param cut_off : A number of the value for prediction, default is 0.5. (i.e. assign value 1 for predictions above 0.5 and value 0 for prediction below or equal to 0.5)
 #' @return A list of the following attributes: `confusion_matrix`, `prevalence`, `accuracy`, `sensitivity`, `specificity`, `false_discovery_rate`, `diagnostic_odds_ratio`
 #' @author Group 11
-#' @importFrom stats runif
+#' @importFrom
 #' @export
-#' @examples 
-#' # Generate synthetic data
-#' set.seed(123)
-#' n <- 100
-#' X <- cbind(1, rnorm(n))  # Assuming X includes a column of 1's for the intercept
-#' beta <- c(-1, 2)         # Coefficients (intercept = -1, slope = 2)
-#' y <- ifelse((1 / (1 + exp(-X %*% beta))) > 0.5, 1, 0)  # Generate binary labels
+#' @examples
+#' #data obtained from https://stats.oarc.ucla.edu/r/dae/logit-regression/
+#' test_data <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
+#'   test_resp <- 1
+#'   test_pred <- 2:4
 #' # Call your confusion_matrix function
-#' result <- confusion_matrix(beta, X, y, cut_off = 0.5)
+#' result <- confusion_matrix(Data = test_data, X=test_pred, y=test_resp, cut_off = 0.5)
+#' print(result)
 
 
-confusion_matrix <- function(beta, X, y, cut_off = 0.5) {
+confusion_matrix <- function(Data, X, y, cut_off = 0.5) {
+
+  #calculate the beta values
+  Data <- cbind(as.matrix(Data[,y]),rep(1, nrow(Data)), as.matrix(Data[,X]))
+
+  beta <- log_betas(Data = Data, X = X, Y = y)$par
+
+
   # first of all Calculate the predicted probabilities
-  predicted_probs <- (1 / (1 + exp(-X %*% beta)))
+  predicted_probs <- (1 / (1 + exp(-(Data[,c(2,X)]) %*% beta)))
 
   # Convert continuous probabilities to binary classification labels
   predicted_labels <- ifelse(predicted_probs > cut_off, 1, 0)
-  
+
   # Compare our predictions and our responses y
-  
-  TP <- sum(y == 1 & predicted_labels == 1)
-  FP <- sum(y == 0 & predicted_labels == 1)
-  
-  FN <- sum(y == 1 & predicted_labels == 0)
-  TN <- sum(y == 0 & predicted_labels == 0)
+
+  TP <- sum(Data[,y] == 1 & predicted_labels == 1)
+  FP <- sum(Data[,y] == 0 & predicted_labels == 1)
+
+  FN <- sum(Data[,y] == 1 & predicted_labels == 0)
+  TN <- sum(Data[,y] == 0 & predicted_labels == 0)
   total_positive <- TP + FN
   total_negative <- FP + TN
-  
+
   # Create the list for result
   confuse_matrix <- matrix(c(TN, FP, FN, TP), nrow = 2)
   rownames(confuse_matrix) <-  c("Actual Positive", "Actual Negative")
   colnames(confuse_matrix) <-  c("Predicted Positive", "Predicted Negative")
-  
+
   prevalence <- total_positive / (total_positive + total_negative)
   accuracy <- (TP + TN) / (total_positive + total_negative)
   sensitivity <- TP / (TP + FN)
   specificity <- TN / (TN + FP)
   false_discovery_rate <- FP / (FP + TP)
   diagnostic_odds_ratio <- (TP * TN) / (FP * FN)
-  
+
   # Create the list and return it
   final_confusion_matrix <- list(
     confuse_matrix = confuse_matrix,
--- log.reg.11/R/part 4 plot of the confusion metrics.R
+++ log.reg.11/R/part 4 plot of the confusion metrics.R
@@ -1,32 +1,30 @@
-#' @title plot for “Confusion Matrix” 
+#' @title plot for “Confusion Matrix”
 #' @description The possibility for the user to plot of any of the above metrics evaluated over a grid of cut-off values for prediction going from 0.1 to 0.9 with steps of 0.1.
-#' @param  beta: A  \code{matrix} to denote the coefficient vector.
-#' @param X:  A  \code{coloumn vector} containing the predictor.
-#' @param y : A  \code{coloumn vector} containing the responses.
+#' @param  Data: A  \code{matrix} containing the data we want to perform logistic regression on.
+#' @param X:  A  \code{vector} denoting the columns of \code{Data} representing the predictor variables.
+#' @param y : A  \code{integer} denoting the column of \code{Data} representing the response variable.
 #' @param value_name : A character, representing the user's choice of which value in the matrix user wants to plot. It needs to be one of six `prevalence`, `accuracy`, `sensitivity`, `specificity`, `false_discovery_rate`, `diagnostic_odds_ratio` , the default is "prevalence"
-#' @return A plot of the value choiced from user versus cut_off_value(from0.1 to 0.9 with step of 0.1)
+#' @return A plot of the value chosen from user versus cut_off_value(from0.1 to 0.9 with step of 0.1)
 #' @author Group 11
-#' @importFrom stats runif
+#' @importFrom
 #' @export
-#' @examples 
-#' # Generate synthetic data
-#' set.seed(123)
-#' n <- 100
-#' X <- cbind(1, rnorm(n))  # Assuming X includes a column of 1's for the intercept
-#' beta <- c(-1, 2)         # Coefficients (intercept = -1, slope = 2)
-#' y <- ifelse((1 / (1 + exp(-X %*% beta))) > 0.5, 1, 0)  # Generate binary labels
-#' # Call your confusion_matrix function
-#' result <- confusion_matrix(beta, X, y, cut_off = 0.5)
-#' plot_confusion_matrix(y,X,beta, value_name = "accuracy")
-plot_confusion_matrix <- function(y, X, beta, value_name = "prevalence"){
+#' @examples
+#' #data obtained from https://stats.oarc.ucla.edu/r/dae/logit-regression/
+#' test_data <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
+#'   test_resp <- 1
+#'   test_pred <- 2:4
+#' result <- confusion_matrix(Data = test_data, X= test_pred, y = test_resp, cut_off = 0.5)
+#' plot_confusion_matrix(Data = test_data, y = test_resp,X = test_pred , value_name = 'false_discovery_rate')
+plot_confusion_matrix <- function(Data, y, X, value_name = "prevalence"){
   cut_off_value <- seq(0.1,0.9, by = 0.1)
-  
+
+
   confusion_matrix_value <- matrix(NA, nrow = 6, ncol = 9)
   rownames(confusion_matrix_value) <- c("prevalence","accuracy", "sensitivity", "specificity", "false_discovery_rate","diagnostic_odds_ratio")
   colnames(confusion_matrix_value) <- seq(0.1, 0.9, by=0.1)
 
   for(i in 1:9){
-      result <- confusion_matrix(beta, X, y, cut_off = cut_off_value[i])
+      result <- confusion_matrix(Data = Data, X = X, y = y, cut_off = cut_off_value[i])
       confusion_matrix_value[1,i] <- result$prevalence
       confusion_matrix_value[2,i] <- result$accuracy
       confusion_matrix_value[3,i] <- result$sensitivity
@@ -37,7 +35,7 @@ plot_confusion_matrix <- function(y, X, beta, value_name = "prevalence"){
     if(value_name %in% rownames(confusion_matrix_value)){
     selected_data <- confusion_matrix_value[value_name,]
     main_title <- paste(value_name, "vs", "cut_off_value" )
-    plot(selected_data,cut_off_value, type = "p", col = "blue" 
+    plot(selected_data,cut_off_value, type = "p", col = "blue"
          , xlab="cut-off value"
          , ylab= value_name
          , main = main_title)
@@ -45,7 +43,7 @@ plot_confusion_matrix <- function(y, X, beta, value_name = "prevalence"){
     } else {
       cat(value_name, "do not exist.\n")
     }
-}   
+}
 
 
 # Reference：https://chat.openai.com/share/d17d0b56-4d16-4200-8ab9-668d61c9ac1b
